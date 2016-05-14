6dc3e354838df69647c91ff8678bc7a0eb35a411
Eduardo Klosowski <eduardo_klosowski@yahoo.com>
2016-05-14 14:28:59 -0300

    SQLAlchemy - Criado tabela Grupo

--- SQLAlchemy/minhacaixa/models.py
+++ SQLAlchemy/minhacaixa/models.py
@@ -1,4 +1,14 @@
+from sqlalchemy import Column, Integer, Sequence, String
 from sqlalchemy.ext.declarative import declarative_base
 
 
 Base = declarative_base()
+
+
+class Grupo(Base):
+    __tablename__ = 'grupo'
+
+    codigo = Column(Integer, Sequence('grupo_codigo_seq'), primary_key=True)
+    nome = Column(String(50))
+    razao_social = Column(String(50))
+    cnpj = Column(String(20))

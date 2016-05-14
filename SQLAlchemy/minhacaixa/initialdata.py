9e3fca4a39583ffb81cb047febf5a23770c1c9c2
Eduardo Klosowski <eduardo_klosowski@yahoo.com>
2016-05-14 14:48:15 -0300

    SQLAlchemy - Adicionado dados do Grupo

--- SQLAlchemy/minhacaixa/initialdata.py
+++ SQLAlchemy/minhacaixa/initialdata.py
@@ -1,5 +1,13 @@
-from . import engine
-from .models import Base
+from . import engine, Session
+from .models import Base, Grupo
 
 
 Base.metadata.create_all(engine)
+
+db = Session()
+db.add_all([
+    Grupo(nome='MyBank',
+          razao_social='MyBank International SA',
+          cnpj='11.222.333/0001-44'),
+])
+db.commit()

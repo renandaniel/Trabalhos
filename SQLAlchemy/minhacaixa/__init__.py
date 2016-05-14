55ab360b0e491c4926b2177273e78412af630135
Eduardo Klosowski <eduardo_klosowski@yahoo.com>
2016-05-14 14:28:16 -0300

    SQLAlchemy - Configurado sessão

--- SQLAlchemy/minhacaixa/__init__.py
+++ SQLAlchemy/minhacaixa/__init__.py
@@ -1,5 +1,7 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 import os
 
 
 engine = create_engine(os.environ.get('DATABASE_URL', 'sqlite:///:memory:'), echo=True)
+Session = sessionmaker(bind=engine)
